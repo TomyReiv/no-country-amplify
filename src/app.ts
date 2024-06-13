f28b8e44ddacfaab129414312124cffe89efc93e
Tomas <ravetomas@gmail.com>
2024-06-13 19:21:44 +0200

    commit

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,13 @@ import swaggerSpec from './swagger';
 import apiRouter from './routes/index.router';
 import { errorHandler } from './middleware/ErrorHandler';
 import { httpResponse } from './utils/EnumsError';
+import cors from "cors"
 
 const app = express();
 const HttpResponse = new httpResponse();
 
 app.use(errorHandler);
-
+app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded({extended: true}))
